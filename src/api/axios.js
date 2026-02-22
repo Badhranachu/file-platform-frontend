8bfa646c6834758cbfcd78a92d6e21ca4a43bf3b
badhranachu <www.badhranks678@gmail.com>
2026-02-22 23:43:20 +0530

    Added environment-based API config for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-});
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
   if (token) {
